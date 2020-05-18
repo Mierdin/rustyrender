65b007ad7a62e29879867159398012362be5415b
Matt Oswalt <matt@oswalt.dev>
2020-05-18 15:39:01 -0700

    Got basic shading working
    Signed-off-by: Matt Oswalt <matt@oswalt.dev>

--- examples/lesson2.rs
+++ examples/lesson2.rs
@@ -14,14 +14,12 @@ use image::GenericImageView;
 use image::{ImageBuffer, Pixel, Rgb, imageops};
 use std::fs::File;
 use std::io::BufReader;
-use rustyrender::{line, background, SCALE, WHITE, RED, BLACK, BLUE, GREEN, Vec2f, Vec3f};
+
+use rustyrender::{SCALE, WHITE, RED, BLACK, BLUE, GREEN};
+use rustyrender::{Vec2f, Vec3f};
+use rustyrender::{line, background, normalize, dot, cross, triangle, barycentric};
 
 fn main() {
-    // let mut imgbuf = image::ImageBuffer::new(1000, 1000);
-    // imgbuf = background(BLACK, imgbuf);
-    // let pts: Vec<Vec2f> = vec![Vec2f::new(10.0, 10.0), Vec2f::new(100.0, 30.0), Vec2f::new(190.0, 160.0)];
-
-
     let (models, materials) = tobj::load_obj("african_head.obj", false).expect("Failed to load file");
 
     debug!("# of models: {}", models.len());
@@ -29,7 +27,7 @@ fn main() {
 
     let mesh = &models[0].mesh;
 
-    let mut imgbuf = image::ImageBuffer::new(SCALE+1, SCALE+1);
+    let mut imgbuf = image::ImageBuffer::new(800+1, 800+1);
     imgbuf = background(BLACK, imgbuf);
 
     // num_face_indices is just a vector which stores the number of indices used by each face.
@@ -44,39 +42,41 @@ fn main() {
         debug!("    face[{}] = {:?}", f, face_indices);
 
         let mut screen_coords: Vec<Vec2f> = Vec::with_capacity(3);
+        let mut world_coords: Vec<Vec3f> = Vec::with_capacity(3);
 
         // Loop through the three sides of the face
         for j in 0..3 {
 
-            let world_coords = Vec3f::new(
+            let v = Vec3f::new(
                 -mesh.positions[3 * *face_indices[j] as usize],
                 -mesh.positions[3 * (*face_indices[j] as usize)+1],
                 -mesh.positions[3 * (*face_indices[j] as usize)+2]
             );
+            world_coords.push(v);
 
             // TODO(mierdin): Dynamically retrieve width/height (ran into borrowing)
             // screen_coords[j] = Vec2f::new((world_coords.x+1.0)*200 as f32/2.0, (world_coords.y+1.0)*200 as f32/2.);
-            screen_coords.push(Vec2f::new((world_coords.x+1.0)*1000 as f32/2.0, (world_coords.y+1.0)*1000 as f32/2.));
+            screen_coords.push(Vec2f::new((v.x+1.0)*801 as f32/2.0, (v.y+1.0)*801 as f32/2.));
 
 
         }
 
-        let mut rng = rand::thread_rng();
-        let rndcolor: Rgb<u8> = image::Rgb([rng.gen_range(0, 255), rng.gen_range(0, 255), rng.gen_range(0, 255)]);
-        println!("COLOR - {:?}", rndcolor);
-        imgbuf = triangle(&screen_coords, rndcolor, imgbuf);
 
-        // for (int i=0; i<model->nfaces(); i++) { 
-        //     std::vector<int> face = model->face(i); 
-        //     Vec2i screen_coords[3]; 
+        // The normal to the triangle can be calculated simply as the cross product of its two sides.
+        let n = cross(world_coords[2]-world_coords[0], world_coords[1]-world_coords[0]);
+        // We then have to set the magnitude of this vector to 1.
+        let n = normalize(n);
 
-        //     Loop through the three vertices in the face
-        //     for (int j=0; j<3; j++) { 
-        //         Vec3f world_coords = model->vert(face[j]); 
-        //         screen_coords[j] = Vec2i((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.); 
-        //     } 
-        //     triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(rand()%255, rand()%255, rand()%255, 255)); 
-        // }
+        let light_dir = Vec3f::new(0.,0.,-1.);
+
+        // the intensity of illumination is equal to the scalar product (aka dot product) of the light vector
+        // and the normal to the given triangle.
+        let intensity = dot(n, light_dir);
+        if  intensity > 0. {
+            // TODO(mierdin): This originally had four numbers - Rgba?
+            let color: Rgb<u8> = image::Rgb([(intensity*255.0) as u8, (intensity*255.0) as u8, (intensity*255.0) as u8]);
+            imgbuf = triangle(&screen_coords, color, imgbuf); 
+        }
 
         next_face = end;
     }
@@ -90,119 +90,3 @@ fn main() {
     imgbuf.save("head_fill.png").unwrap();
 }
 
-
-// This is the first method taught in lesson 2. I didn't quite get there, and it seems this is the less preferable
-// approach anyways. Will move on to barycentric approach, and perhaps revisit this if there's value and I have time.
-pub fn triangle_enumerate_lines(mut v0: Vec2f, mut v1: Vec2f, mut v2: Vec2f, color: Rgb<u8>, mut imgbuf: ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-
-    imgbuf = line(v0, v1, WHITE, imgbuf); 
-    imgbuf = line(v1, v2, WHITE, imgbuf); 
-    imgbuf = line(v2, v0, WHITE, imgbuf); 
-
-    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!)
-    if v0.y>v1.y { mem::swap(&mut v0, &mut v1) };
-    if v0.y>v2.y { mem::swap(&mut v0, &mut v2) }; 
-    if v1.y>v2.y { mem::swap(&mut v1, &mut v2) };
-
-    // imgbuf = line(v0, v1, BLUE, imgbuf); 
-    // imgbuf = line(v1, v2, BLUE, imgbuf); 
-    // imgbuf = line(v2, v0, BLUE, imgbuf); 
-
-
-    println!("###############################################");
-    println!("#############   NEW TRIANGLE   ################");
-    println!("###############################################");
-
-    let total_height = v2.y-v0.y;  // Largest Y minus smallest Y
-    let mut y = v0.y;  // start at the bottom
-
-    // Go up until we reach the middle Y
-    while y <= v1.y {
-        let segment_height = v1.y-v0.y+1.0;
-        println!("--------------------------------------");
-        println!("y | {}", y);
-        println!("v0x {} | v0y {} | v1x {} | v1y {}", v0.x, v0.y, v1.x, v1.y);
-        println!("total_height {} | segment_height {}", total_height, segment_height);
-        let alpha = (y-v0.y)/total_height;
-        let beta = (y-v0.y)/segment_height; // be careful with divisions by zero 
-
-        println!("alpha {} | beta {}", alpha, beta);
-
-        // My attempt
-        // TODO(Mierdin): figure out how to implement multiply for Vec2f so you don't have to do this separately here
-        let A: Vec2f = v0 + (v2-v0);
-        let B: Vec2f = v0 + (v1-v0);
-        println!("FIRST | Ax {} | Ay {} | Bx {} | By {}", A.x, A.y, B.x, B.y);
-        let A = Vec2f::new(A.x*alpha, A.y);
-        let B = Vec2f::new(B.x*beta, B.y);
-        // Original C++
-        // Vec2i A = t0 + (t2-t0)*alpha; 
-        // Vec2i B = t0 + (t1-t0)*beta; 
-
-        println!("SECOND | Ax {} | Ay {} | Bx {} | By {}", A.x, A.y, B.x, B.y);
-
-        imgbuf.put_pixel(A.x as u32, y as u32, RED);
-        imgbuf.put_pixel(B.x as u32, y as u32, GREEN);
-
-        y += 1.0;
-    }
-
-    imgbuf
-}
-
-fn cross(v1: Vec3f, v2: Vec3f) -> Vec3f {
-    return Vec3f::new(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x)
-}
-
-fn barycentric(pts: &Vec<Vec2f>, P: &Vec2f) -> Vec3f {
-    let u = cross(Vec3f::new(pts[2].x-pts[0].x, pts[1].x-pts[0].x, pts[0].x-P.x), Vec3f::new(pts[2].y-pts[0].y, pts[1].y-pts[0].y, pts[0].y-P.y));
-
-    /* `pts` and `P` has integer value as coordinates
-       so `abs(u[2])` < 1 means `u[2]` is 0, that means
-       triangle is degenerate, in this case return something with negative coordinates */
-    // TODO(mierdin): This is the original statement from the C++ code. This **may** not be needed, but it still works, so /shrug
-    if u.z.abs()<1.0 { return Vec3f::new(-1.0,1.0,1.0) };
-    let ret = Vec3f::new(1.0-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
-    // println!("Barycentric is returning: {:?}", ret);
-
-    ret
-}
-
-
-fn triangle(pts: &Vec<Vec2f>, color: Rgb<u8>, mut imgbuf: ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-
-    let mut bboxmin = Vec2f::new((imgbuf.width()-1) as f32, (imgbuf.height()-1) as f32);
-    let mut bboxmax = Vec2f::new(0.0, 0.0);
-    let clamp = Vec2f::new((imgbuf.width()-1) as f32, (imgbuf.height()-1) as f32);
-
-    for i in 0..3 {
-        bboxmin.x = (0.0 as f32).max(bboxmin.x.min(pts[i].x)); 
-        bboxmax.x = clamp.x.min(bboxmax.x.max(pts[i].x)); 
-
-        bboxmin.y = (0.0 as f32).max(bboxmin.y.min(pts[i].y)); 
-        bboxmax.y = clamp.y.min(bboxmax.y.max(pts[i].y)); 
-    }
-
-    let mut P = Vec2f::new(bboxmin.x, bboxmin.y);
-    // println!("bboxmin.x {} | bboxmin.y {}", bboxmin.x, bboxmin.y);
-    // println!("bboxmax.x {} | bboxmax.y {}", bboxmax.x, bboxmax.y);
-    while P.x <= bboxmax.x {
-        // println!("P.x {}", P.x);
-
-        // Reset P.y before each loop
-        P.y = bboxmin.y;
-        while P.y <= bboxmax.y {
-            // println!("P.y {}", P.y);
-            let bc_screen = barycentric(&pts, &P);
-            if bc_screen.x<0.0 || bc_screen.y<0.0 || bc_screen.z<0.0 {
-                P.y += 1.0;
-                continue
-            }; 
-            imgbuf.put_pixel(P.x as u32, P.y as u32, color);
-            P.y += 1.0;
-        }
-        P.x += 1.0;
-    }
-
-    imgbuf
-}
--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub const BLUE: Rgb<u8> = image::Rgb([0, 0, 255]);
 
 // Creating a single value for controlling scale. This sets the dimensions of the imagebuffer,
 // but is also used to determine how often to draw a pixel, and for how long
-pub const SCALE: u32 = 1000;
+pub const SCALE: u32 = 800;
 
 #[derive(Copy, Clone, Debug)]
 pub struct Vec2f {
@@ -63,6 +63,49 @@ impl Vec3f {
     pub fn new(x: f32, y: f32, z: f32) -> Vec3f {
         Vec3f { x: x, y: y, z: z }
     }
+
+    pub fn pow(&self, second: Vec3f) -> Vec3f {
+        let v = Vec3f::new(
+            // self.x.powf(second.x),
+            // self.y.powf(second.y),
+            // self.z.powf(second.z),
+
+            // NOPE.
+            (self.x as u32 ^ second.x as u32) as f32,
+            (self.y as u32 ^ second.y as u32) as f32,
+            (self.z as u32 ^ second.z as u32) as f32,
+        );
+
+        v
+    }
+
+
+    // float norm() { return std::sqrt(x*x+y*y+z*z); }
+    // vec<3,T> & normalize(T l=1) { *this = (*this)*(l/norm()); return *this; }
+    // http://www.fundza.com/vectors/normalize/
+
+
+
+    pub fn normalize(mut self) {
+
+        // return sqrt(this.x*this.x + this.y*this.y);
+
+        self.x = self.x * (1. / (self.x*self.x+self.y*self.y+self.z*self.z).sqrt());
+        self.y = self.y * (1. / (self.x*self.x+self.y*self.y+self.z*self.z).sqrt());
+        self.z = self.z * (1. / (self.x*self.x+self.y*self.y+self.z*self.z).sqrt());
+    }
+}
+
+impl Sub for Vec3f {
+    type Output = Vec3f;
+
+    fn sub(self, other: Vec3f) -> Vec3f {
+        Vec3f {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
 }
 
 
@@ -86,10 +129,92 @@ pub fn line(v0: Vec2f, v1: Vec2f, color: Rgb<u8>, mut imgbuf: ImageBuffer<Rgb<u8
 
 }
 
+pub fn normalize(v1: Vec3f) -> Vec3f {
+    let retvec = Vec3f::new(
+        v1.x * ( 1.0 / dot(v1, v1)).sqrt(),
+        v1.y * ( 1.0 / dot(v1, v1)).sqrt(),
+        v1.z * ( 1.0 / dot(v1, v1)).sqrt(),
+    );
+
+    retvec
+}
+
 // Writes all pixels in an image buffer with the same color
 pub fn background(color: Rgb<u8>, mut imgbuf: ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     for (_x, _y, pixel) in imgbuf.enumerate_pixels_mut() {
         *pixel = color;
+    }
+
+    imgbuf
+}
+
+
+// This function calculuates the cross product of two vectors
+// http://sites.science.oregonstate.edu/math/home/programs/undergrad/CalculusQuestStudyGuides/vcalc/crossprod/crossprod.html
+pub fn cross(v1: Vec3f, v2: Vec3f) -> Vec3f {
+    return Vec3f::new(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x)
+}
+
+// Also referred to as a scalar product
+// https://www.mathsisfun.com/algebra/vectors-dot-product.html
+pub fn dot(v1: Vec3f, v2: Vec3f) -> f32 {
+
+    // a · b = ax × bx + ay × by
+
+    // (v1.x * v2.x) + (v1.y * v2.y) // + (v1.z * v2.z);
+
+
+    v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+}
+
+pub fn barycentric(pts: &Vec<Vec2f>, P: &Vec2f) -> Vec3f {
+    let u = cross(Vec3f::new(pts[2].x-pts[0].x, pts[1].x-pts[0].x, pts[0].x-P.x), Vec3f::new(pts[2].y-pts[0].y, pts[1].y-pts[0].y, pts[0].y-P.y));
+
+    /* `pts` and `P` has integer value as coordinates
+       so `abs(u[2])` < 1 means `u[2]` is 0, that means
+       triangle is degenerate, in this case return something with negative coordinates */
+    // TODO(mierdin): This is the original statement from the C++ code. This **may** not be needed, but it still works, so /shrug
+    if u.z.abs()<1.0 { return Vec3f::new(-1.0,1.0,1.0) };
+    let ret = Vec3f::new(1.0-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+    // println!("Barycentric is returning: {:?}", ret);
+
+    ret
+}
+
+
+pub fn triangle(pts: &Vec<Vec2f>, color: Rgb<u8>, mut imgbuf: ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+
+    let mut bboxmin = Vec2f::new((imgbuf.width()-1) as f32, (imgbuf.height()-1) as f32);
+    let mut bboxmax = Vec2f::new(0.0, 0.0);
+    let clamp = Vec2f::new((imgbuf.width()-1) as f32, (imgbuf.height()-1) as f32);
+
+    for i in 0..3 {
+        bboxmin.x = (0.0 as f32).max(bboxmin.x.min(pts[i].x)); 
+        bboxmax.x = clamp.x.min(bboxmax.x.max(pts[i].x)); 
+
+        bboxmin.y = (0.0 as f32).max(bboxmin.y.min(pts[i].y)); 
+        bboxmax.y = clamp.y.min(bboxmax.y.max(pts[i].y)); 
+    }
+
+    let mut P = Vec2f::new(bboxmin.x, bboxmin.y);
+    // println!("bboxmin.x {} | bboxmin.y {}", bboxmin.x, bboxmin.y);
+    // println!("bboxmax.x {} | bboxmax.y {}", bboxmax.x, bboxmax.y);
+    while P.x <= bboxmax.x {
+        // println!("P.x {}", P.x);
+
+        // Reset P.y before each loop
+        P.y = bboxmin.y;
+        while P.y <= bboxmax.y {
+            // println!("P.y {}", P.y);
+            let bc_screen = barycentric(&pts, &P);
+            if bc_screen.x<0.0 || bc_screen.y<0.0 || bc_screen.z<0.0 {
+                P.y += 1.0;
+                continue
+            }; 
+            imgbuf.put_pixel(P.x as u32, P.y as u32, color);
+            P.y += 1.0;
+        }
+        P.x += 1.0;
     }
 
     imgbuf
